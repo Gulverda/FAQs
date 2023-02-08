db94d7ac36175ed5dca3ef7a75ca493aee6be97c
luka gulverdashvili <109980584+Gulverda@users.noreply.github.com>
2023-02-09 03:15:35 +0400

    Update app.js

--- app.js
+++ app.js
@@ -4,39 +4,32 @@ const faqs = document.querySelectorAll(".faq");
 
 function handleCloseFaq() {
   faqs.forEach((faq) => {
-    faq.classList.remove("active");
+    faq.classList.toggle("active");
+    if (faq.classList.contains("active")) {
+      faq.classList.remove("active");
+    } else {
+      faq.classList.replace("unactive");
+    }
   });
 }
 
 faqs.forEach((faq) => {
   faq.addEventListener("click", () => {
     handleCloseFaq();
-
-    faq.classList.add("active");
+    faq.classList.toggle("active");
   });
 });
 
-//toglle and remove active class
-
-// acc_btns.forEach((btn) => {
-//     btn.addEventListener("click", () => {
-        
-//         const content = btn.nextElementSibling;
-//         const active = document.querySelector(".active");
-
-//         if (active) {
-//             active.classList.toggle("active");
-//             active.style.maxHeight = 0;
-//         } else {
-//         content.classList.toggle("active");
-//         content.style.maxHeight = content.scrollHeight + "px";}
-//     })
-// })
-
-//Toggle active class2
-
-// for (let i = 0; i < faqs.length; i++) {
-//     faqs[i].addEventListener("click", () => {
-//         faqs[i].classList.toggle("none");
-//     })
+// function handleCloseFaq() {
+//   faqs.forEach((faq) => {
+//     faq.classList.remove("active");
+//   });
 // }
+
+// faqs.forEach((faq) => {
+//   faq.addEventListener("click", () => {
+//     handleCloseFaq();
+
+//     faq.classList.add("active");
+//   });
+// });
