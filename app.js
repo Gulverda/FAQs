bbca488c92126c341d3060e985f9987a8fc0b45b
luka gulverdashvili <109980584+Gulverda@users.noreply.github.com>
2023-02-09 17:53:23 +0400

    Bug/Fixes

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const acc_btns = document.querySelectorAll(".kitxva");
-const acc_contents = document.querySelectorAll(".answer");
+const question = document.querySelectorAll(".kitxva");
+const answer = document.querySelectorAll(".answer");
 const faqs = document.querySelectorAll(".faq");
+
+
+
 
 function handleCloseFaq() {
   faqs.forEach((faq) => {
     faq.classList.toggle("active");
     if (faq.classList.contains("active")) {
-      faq.classList.remove("active");
+      faq.classList.toggle("active");
+    } else if (faq.classList.toggle("unactive")){
+      faq.classList.toggle("active");
     } else {
-      faq.classList.replace("unactive");
+        faq.classList.remove("unactive");
     }
   });
 }
@@ -16,20 +21,31 @@ function handleCloseFaq() {
 faqs.forEach((faq) => {
   faq.addEventListener("click", () => {
     handleCloseFaq();
-    faq.classList.toggle("active");
+    faq.classList.add("active");
   });
 });
 
-// function handleCloseFaq() {
-//   faqs.forEach((faq) => {
-//     faq.classList.remove("active");
-//   });
+//toglle and remove active class
+
+// acc_btns.forEach((btn) => {
+//     btn.addEventListener("click", () => {
+        
+//         const content = btn.nextElementSibling;
+//         const active = document.querySelector(".active");
+
+//         if (active) {
+//             active.classList.toggle("active");
+//             active.style.maxHeight = 0;
+//         } else {
+//         content.classList.add("active");
+//         content.style.maxHeight = content.scrollHeight + "px";}
+//     })
+// })
+
+//Toggle active class2
+
+// for (let i = 0; i < faqs.length; i++) {
+//     faqs[i].addEventListener("click", () => {
+//         faqs[i].classList.toggle("active");
+//     })
 // }
-
-// faqs.forEach((faq) => {
-//   faq.addEventListener("click", () => {
-//     handleCloseFaq();
-
-//     faq.classList.add("active");
-//   });
-// });
